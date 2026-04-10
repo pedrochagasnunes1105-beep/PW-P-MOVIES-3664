5b32cbd12345739eaf9b3ab7ac3db104e79777fb
pedro <pedro.n@ipvc.pt>
2026-04-10 10:27:42 +0100

    Trabalho movies

---
+++ src/prisma/prismaClient.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
+
+module.exports = prisma;
